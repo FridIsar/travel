b65ecb25eeec5ba4278f20800ff87f007420787c
Isar <isarfridriksson@gmail.com>
2023-08-04 04:29:23 +0000

    last seen, nav bar and docs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 import React, { useEffect, useState } from 'react';
 import { createClient } from '@supabase/supabase-js'
+import L from 'leaflet';
+import iconMarker from 'leaflet/dist/images/marker-icon.png'
+import iconRetina from 'leaflet/dist/images/marker-icon-2x.png'
+import iconShadow from 'leaflet/dist/images/marker-shadow.png'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+const icon = L.icon({ 
+  iconRetinaUrl:iconRetina, 
+  iconUrl: iconMarker, 
+  shadowUrl: iconShadow 
+});
 const supabaseUrl = 'https://rjpqxbwlcnkylrndqvcb.supabase.co'
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqcHF4YndsY25reWxybmRxdmNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTExMTQ4MDEsImV4cCI6MjAwNjY5MDgwMX0.jt9JRLIK5SR5Zj7pMiBZnxHKTNHaKpIYk8_TWOhvw8c"//process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
@@ -15,7 +28,7 @@ function App() {
       try {
         let { data: Positions } = await supabase
           .from('Positions')
-          .select('coordinates')
+          .select('*')
           .order('id', { ascending: false })
           .limit(1);
         setPosition(Positions);
@@ -29,12 +42,25 @@ function App() {
 
   return (
     <div className="App">
+      
+          <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Travel</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="https://drive.google.com/drive/folders/1jKcCn31nJrKqbhQh-kzhqYpP7yh1FcNo">Docs</Nav.Link>
+            <Nav.Link href="#link">Last seen {position[0].created_at}</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
       {position ? (
         <MapContainer center={position[0].coordinates} zoom={13} style={{ height: '100vh' }}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <Marker position={position[0].coordinates}>
+        <Marker position={position[0].coordinates} icon={icon}>
           <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+            Hi :) <br /> I should be around there!
           </Popup>
         </Marker>
       </MapContainer>
