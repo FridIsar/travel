8551c530669002f3c6a005a82bd6724206ea41eb
Isar <isarfridriksson@gmail.com>
2023-08-04 04:34:35 +0000

    test

--- src/App.js
+++ src/App.js
@@ -42,19 +42,6 @@ function App() {
 
   return (
     <div className="App">
-      
-          <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">Travel</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="https://drive.google.com/drive/folders/1jKcCn31nJrKqbhQh-kzhqYpP7yh1FcNo">Docs</Nav.Link>
-            <Nav.Link href="#link">Last seen {position[0].created_at}</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
       {position ? (
         <MapContainer center={position[0].coordinates} zoom={13} style={{ height: '100vh' }}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
